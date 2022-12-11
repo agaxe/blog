22e032598aff2a9ce35ef3fc5d75e2e75b27a9b8
agaxe <agaxe.dev@gmail.com>
2022-12-11 20:29:39 +0900

    update: formatDate props:dateInfo type 수정

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,4 +1,4 @@
-export const formatDate = (dateInfo: Date | number, options = {}) => {
+export const formatDate = (dateInfo: Date | number | string, options = {}) => {
   const date = new Date(dateInfo);
 
   return new Intl.DateTimeFormat('ko-KR', {
