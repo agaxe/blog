218ff0922bd55cbf38d24d99feec8ade1c2294a8
agaxe <agaxe.dev@gmail.com>
2022-12-12 01:19:36 +0900

    update: Layout 컴포넌트 - section 스타일 추가

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 
@@ -6,8 +7,13 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header />
-      {children}
+      <Section>{children}</Section>
       <Footer />
     </div>
   );
 };
+
+const Section = styled.section`
+  padding-top: 32px;
+  padding-bottom: 32px;
+`;
