b2adbe242f4fd5a59b7bafba4e78d83d46fbddc8
agaxe <agaxe.dev@gmail.com>
2022-12-14 01:29:33 +0900

    feat: next-optimized-images 패키지 적용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '/'
+  }
   //swcMinify: true
 };
 
