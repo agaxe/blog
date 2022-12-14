61d5ddb6ea9694292aeea24e48cb169ceb1c8369
agaxe <agaxe.dev@gmail.com>
2022-12-15 00:30:08 +0900

    feat: 404 에러 페이지 생성

---
+++ pages/404/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+
+export default function NotFound() {
+  return (
+    <S.Wrap>
+      <S.ContentBox>
+        <S.Title>404</S.Title>
+        <S.Desc>Page Not Found</S.Desc>
+      </S.ContentBox>
+    </S.Wrap>
+  );
+}
---
+++ pages/404/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContentBox = styled.div`
+  text-align: center;
+  display: grid;
+  gap: 16px;
+  user-select: none;
+`;
+
+export const Title = styled.h1`
+  font-size: 4.5rem;
+  font-weight: 700;
+`;
+
+export const Desc = styled.p`
+  font-weight: 300;
+  font-size: 1.5rem;
+  color: var(--color-gray);
+`;
