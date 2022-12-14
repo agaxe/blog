94f0ba24e4beea1bd666bd24a17446ac60028d17
agaxe <agaxe.dev@gmail.com>
2022-12-14 19:12:44 +0900

    update: Image 컴포넌트 - img 태그 적용

--- components/common/Image.tsx
+++ components/common/Image.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import NextImage, { ImageProps } from 'next/image';
 
 export const Image = (props: ImageProps) => {
-  return <NextImage {...props} />;
+  // return <NextImage {...props} />;
+  // eslint-disable-next-line @next/next/no-img-element
+  return <Img src={`/blog${String(props.src)}`} alt={props.alt} />;
 };
+
+const Img = styled.img`
+  width: inherit;
+  height: inherit;
+  object-fit: cover;
+`;
