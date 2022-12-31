f2b538fe1b18db15782f5eafd81ab9ed5fb69707
agaxe <agaxe.dev@gmail.com>
2023-01-01 00:43:50 +0900

    update: 메인, 태그 페이지 - 페이지 로딩 시 스켈레톤 UI 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { GetStaticProps } from 'next';
 import styled from 'styled-components';
 import { Layout } from '@/components/layout/Layout';
 import { MainHeader } from '@/components/layout/MainHeader';
 import { NotionPageList } from '@/components/notion/NotionPageList';
+import { NotionPageListSkeleton } from '@/components/notion/NotionPageList/Skeleton';
 import { usePageItems } from '@/hooks/usePageItems';
 import { PageItemsReturnType, getDatabaseItems } from '@/lib/notion/pages';
 import { ISR_REVALIDATE_TIME } from '@/shared/variable';
@@ -20,17 +21,21 @@ export default function Home({ data }: HomeProps) {
     <Layout>
       <HomePage>
         <MainHeader />
-        {data.results.length ? (
-          <NotionPageList data={items} />
-        ) : (
-          <div>데이터가 존재하지 않습니다.</div>
-        )}
-        <div
-          ref={baseRef}
-          style={{
-            display: items.length && pagination.hasMore ? 'block' : 'none'
-          }}
-        />
+        <div>
+          {data.results.length ? (
+            <NotionPageList data={items} />
+          ) : (
+            <div>데이터가 존재하지 않습니다.</div>
+          )}
+          <div
+            ref={baseRef}
+            style={{
+              display: items.length && pagination.hasMore ? 'block' : 'none'
+            }}
+          >
+            <NotionPageListSkeleton />
+          </div>
+        </div>
       </HomePage>
     </Layout>
   );
--- pages/tags/[tagName].tsx
+++ pages/tags/[tagName].tsx
@@ -5,6 +5,7 @@ import { Seo } from '@/components/common/Seo';
 import { Layout } from '@/components/layout/Layout';
 import { TagHeader } from '@/components/layout/TagHeader';
 import { NotionPageList } from '@/components/notion/NotionPageList';
+import { NotionPageListSkeleton } from '@/components/notion/NotionPageList/Skeleton';
 import { usePageItems } from '@/hooks/usePageItems';
 import { PageItemsReturnType, getDatabaseItems } from '@/lib/notion/pages';
 import { getPathTagItems } from '@/lib/notion/tags';
@@ -26,13 +27,17 @@ export default function TagName({ tagName, data }: TagNameProps) {
       <Layout>
         <TagNameWrap>
           <TagHeader tagName={convertPascalCase(String(tagName))} />
-          <NotionPageList data={items} />
-          <div
-            ref={baseRef}
-            style={{
-              display: items.length && pagination.hasMore ? 'block' : 'none'
-            }}
-          />
+          <div>
+            <NotionPageList data={items} />
+            <div
+              ref={baseRef}
+              style={{
+                display: items.length && pagination.hasMore ? 'block' : 'none'
+              }}
+            >
+              <NotionPageListSkeleton />
+            </div>
+          </div>
         </TagNameWrap>
       </Layout>
     </>
