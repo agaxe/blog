bcd96534d631f8b2baa18d239d3ebeff1509ea9e
agaxe <agaxe.dev@gmail.com>
2024-09-11 17:37:04 +0900

    test: notion/getPathPages - 테스트 코드 추가

---
+++ lib/notion/pages/__test__/getPathPages.test.ts
@@ -0,0 +1,31 @@
+import mockParsePageItems from '@/mocks/parsePageItems';
+import { parseDatabaseItems } from '@/utils/parseDatabaseItems';
+import { getPathPages } from '../getPathPages';
+
+jest.mock('@/utils/parseDatabaseItems');
+
+describe('getPathPages', () => {
+  it('최대 아이템 10개, 아이템 수 10개 -> 페이지 수는 1개', async () => {
+    (parseDatabaseItems as jest.Mock).mockReturnValue(mockParsePageItems);
+
+    const result = await getPathPages();
+
+    expect(result).toEqual([{ params: { pageNum: '1' } }]);
+  });
+
+  it('최대 아이템 10개, 아이템 수 23개 -> 페이지 수는 3개', async () => {
+    (parseDatabaseItems as jest.Mock).mockReturnValue([
+      ...mockParsePageItems,
+      ...mockParsePageItems,
+      ...mockParsePageItems.filter((_, i) => i < 3)
+    ]);
+
+    const result = await getPathPages();
+
+    expect(result).toEqual([
+      { params: { pageNum: '1' } },
+      { params: { pageNum: '2' } },
+      { params: { pageNum: '3' } }
+    ]);
+  });
+});
