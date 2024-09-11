0be7a831331d706a482386dfbe7cf98550d81633
agaxe <agaxe.dev@gmail.com>
2024-09-11 17:40:18 +0900

    test: notion API 라이브러리 - 페이지 리스트 mock 데이터 추가

---
+++ mocks/notionPageList.ts
@@ -0,0 +1,12 @@
+import mockPageItems from './notionPageItems';
+
+const list = {
+  has_more: false,
+  next_cursor: null,
+  object: 'list',
+  page_or_database: {},
+  request_id: '3fbb87dc-01f6-44ab-8623-3ef526373a41',
+  results: mockPageItems
+};
+
+export default list;
