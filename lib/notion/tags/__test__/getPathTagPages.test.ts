06dae4edbf81c481b6be309dc737413fa62c62ec
agaxe <agaxe.dev@gmail.com>
2024-09-19 11:44:48 +0900

    test: notion/getPathTagPages - 임시 테스트 코드에 skip 적용

--- lib/notion/tags/__test__/getPathTagPages.test.ts
+++ lib/notion/tags/__test__/getPathTagPages.test.ts
@@ -1,5 +1,5 @@
 import mockNotionDB from '@/mocks/notionDB';
-// import { getPathTagPages } from '../getPathTagPages';
+import { getPathTagPages } from '../getPathTagPages';
 import { getTagItems } from '../getTagItems';
 
 jest.mock('@/lib/notion/tags/getTagItems');
@@ -9,40 +9,35 @@ jest.mock('@/shared/variable', () => ({
 
 const dbTagItems = mockNotionDB.properties.tags.multi_select.options;
 
-describe('getPathTagPages', () => {
+describe.skip('getPathTagPages', () => {
   beforeAll(() => {
     (getTagItems as jest.Mock).mockResolvedValue(dbTagItems);
   });
 
   //TODO: 리팩토링 시 해당 테스트에 맞춰 코드 변경 필요
-  // it('tagName 값이 존재하지 않는 경우', async () => {
-  //   const res = await getPathTagPages();
+  it('tagName 값이 존재하지 않는 경우', async () => {
+    const res = await getPathTagPages();
 
-  //   expect(res).toEqual([
-  //     { params: { tagName: 'tag name_1-1', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_1-2', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_2-1', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_2-2', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_3-1', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_3-2', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_4-1', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_4-2', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_5-1', pageNum: '2' } },
-  //     { params: { tagName: 'tag name_5-2', pageNum: '2' } },
-  //   ]);
-  // });
+    expect(res).toEqual([
+      { params: { tagName: 'tag name_1-1', pageNum: '2' } },
+      { params: { tagName: 'tag name_1-2', pageNum: '2' } },
+      { params: { tagName: 'tag name_2-1', pageNum: '2' } },
+      { params: { tagName: 'tag name_2-2', pageNum: '2' } },
+      { params: { tagName: 'tag name_3-1', pageNum: '2' } },
+      { params: { tagName: 'tag name_3-2', pageNum: '2' } },
+      { params: { tagName: 'tag name_4-1', pageNum: '2' } },
+      { params: { tagName: 'tag name_4-2', pageNum: '2' } },
+      { params: { tagName: 'tag name_5-1', pageNum: '2' } },
+      { params: { tagName: 'tag name_5-2', pageNum: '2' } }
+    ]);
+  });
 
   //TODO: 리팩토링 시 해당 테스트에 맞춰 코드 변경 필요
-  // it('tagName 값이 존재하는 경우', async () => {
-  //   const res = await getPathTagPages('tag name_1-1');
+  it('tagName 값이 존재하는 경우', async () => {
+    const res = await getPathTagPages('tag name_1-1');
 
-  //   expect(res).toEqual([
-  //     { params: { tagName: 'tag name_1-1', pageNum: '2' } }
-  //   ]);
-  // });
-
-  //!
-  it('', () => {
-    expect(true).toBe(true);
+    expect(res).toEqual([
+      { params: { tagName: 'tag name_1-1', pageNum: '2' } }
+    ]);
   });
 });
