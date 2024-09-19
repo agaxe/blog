0aa0a3c2ddf5114fd707bf650b5396077d9eb81b
agaxe <agaxe.dev@gmail.com>
2024-09-19 14:11:12 +0900

    test: '포스트 아이템' e2e 테스트 코드 추가

---
+++ e2e/common/post-item.spec.ts
@@ -0,0 +1,83 @@
+import { type Locator, expect, test } from '@playwright/test';
+
+const pages = [
+  { name: '메인', url: 'http://127.0.0.1:3000' },
+  { name: '포스트 리스트', url: 'http://127.0.0.1:3000/pages/1' }
+];
+
+pages.forEach((pageItem) => {
+  let firstPostItem: Locator;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(pageItem.url);
+
+    firstPostItem = page.locator(
+      '[class*="styles__Section"] ul:nth-child(2) > li:first-child'
+    );
+  });
+
+  test.describe(`[${pageItem.name}] 페이지 - 포스트 아이템`, async () => {
+    test('포스트 아이템에는 타이틀이 존재한다.', async () => {
+      const title = firstPostItem.locator('[class*="styles__PageTitle"]');
+
+      await expect(title).toBeVisible();
+    });
+
+    test('포스트 아이템에는 포스팅이 작성된 날짜 정보가 정해진 텍스트 포맷으로 존재한다.', async () => {
+      const dateText = firstPostItem.locator('[class*="styles__PageDate"]');
+
+      expect(await dateText.textContent()).toMatch(
+        /\d{4}년\s\d{1,2}월\s\d{1,2}일/
+      );
+    });
+
+    test('포스트 아이템에는 태그 리스트가 존재한다.', async () => {
+      const tagList = firstPostItem.locator('[class*="NotionTagList"]');
+
+      await expect(tagList).toBeVisible();
+    });
+
+    test('포스트 아이템에는 태그 아이템이 1개 이상 존재한다.', async () => {
+      const tagList = firstPostItem.locator('[class*="NotionTagList"]');
+      const tagItem = tagList.locator(':scope > li');
+
+      expect(await tagItem.count()).toBeGreaterThanOrEqual(1);
+    });
+
+    test('포스트 아이템 클릭 시 해당 포스트의 상세 페이지로 이동한다.', async ({
+      page
+    }) => {
+      const mainSection = page.locator('[class*="styles__Section"]');
+      const firstPostLink = mainSection.locator(
+        'ul:nth-child(2) > li:first-child > div > a'
+      );
+      const postLinkHref = await firstPostLink.getAttribute('href');
+
+      await firstPostLink.click();
+
+      await expect(page).toHaveURL(new RegExp(`${postLinkHref}$`), {
+        timeout: 15000
+      });
+    });
+
+    test('포스트 아이템의 태그 클릭 시 해당 태그의 포스트 리스트 페이지로 이동한다.', async ({
+      page
+    }) => {
+      const mainSection = page.locator('[class*="styles__Section"]');
+      const firstPostItem = mainSection.locator(
+        'ul:nth-child(2) > li:nth-child(2)'
+      );
+      const tagLink = firstPostItem.locator(
+        '[class*="NotionTagList"] > li:nth-child(1) a'
+      );
+
+      const tagHref = await tagLink.getAttribute('href');
+
+      await tagLink.click();
+
+      await expect(page).toHaveURL(new RegExp(`${tagHref}$`), {
+        timeout: 15000
+      });
+    });
+  });
+});
