63c9c044adacee7d4f49cfd3a6d7aca57d76eda2
agaxe <agaxe.dev@gmail.com>
2024-09-19 14:11:39 +0900

    test: '사이드 태그 리스트' e2e 테스트 코드 추가

---
+++ e2e/common/side-tag-list.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@playwright/test';
+import { size } from '@/styles/device';
+
+const pages = [
+  { name: '메인', url: 'http://127.0.0.1:3000' },
+  { name: '포스트 리스트', url: 'http://127.0.0.1:3000/pages/1' },
+  {
+    name: '특정 태그 포스트 리스트',
+    url: 'http://127.0.0.1:3000/tags/react/pages/1'
+  }
+];
+
+pages.forEach((pageItem) => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(pageItem.url);
+  });
+
+  test.describe(`[${pageItem.name}] 페이지 - 사이드 태그 리스트`, () => {
+    test('브라우저 넓이가 lg 이하인 경우 사이드 태그 리스트 hide', async ({
+      page
+    }) => {
+      const sideTagList = page.locator(
+        '[class*="styles__Section"] [class*="styles__Wrap"] > ul:first-child'
+      );
+      await expect(sideTagList).toBeVisible();
+
+      await page.setViewportSize({ width: size.lg, height: 600 });
+
+      await expect(sideTagList).not.toBeVisible();
+    });
+  });
+});
