e980d2b3ec3a2850ec5f3a70c9ff12803ca28758
agaxe <agaxe.dev@gmail.com>
2024-09-19 14:10:04 +0900

    test: [태그/리스트] 페이지 - e2e 테스트 코드 추가

---
+++ e2e/tags-page.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://127.0.0.1:3000/tags');
+});
+
+test.describe('[태그 리스트] 페이지', () => {
+  test(`타이틀 텍스트 'Tags' 가 존재한다.`, async ({ page }) => {
+    const title = page.locator(
+      '[class*="styles__Section"] > [class*="styles__wrap"] > h2'
+    );
+
+    await expect(title).toHaveText('Tags');
+  });
+
+  test('태그 리스트가 존재한다.', async ({ page }) => {
+    const tagList = page.locator('[class*="TagList__Wrap"]');
+
+    if ((await tagList.count()) === 0) return;
+
+    await expect(tagList).toBeVisible();
+  });
+
+  test('태그 리스트 아이템이 존재한다.', async ({ page }) => {
+    const tagItem = page.locator('[class*="TagList__TagItem"]');
+
+    if ((await tagItem.count()) === 0) return;
+
+    expect(await tagItem.count()).toBeGreaterThanOrEqual(1);
+  });
+});
