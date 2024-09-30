4eb3f5c23d54fb4aa65e6ef1f75f6392b28c1aa6
agaxe <agaxe.dev@gmail.com>
2024-09-30 15:52:49 +0900

    test: NotionPageItem 컴포넌트 - 테스트 케이스 분리 및 메시지 수정

--- components/notion/NotionPageItem/index.test.tsx
+++ components/notion/NotionPageItem/index.test.tsx
@@ -6,33 +6,59 @@ import parsePageItems from '@/mocks/parsePageItems';
 import { NotionPageItem } from '.';
 
 describe('NotionPageItem 컴포넌트', () => {
-  it('기본 props 체크', () => {
-    const { getByRole, getByText, getAllByRole } = render(
-      <NotionPageItem data={parsePageItems[0]} />
-    );
+  it('포스트 제목이 표시된다.', () => {
+    render(<NotionPageItem data={parsePageItems[0]} />);
 
-    const title = getByRole('heading', { level: 3 });
-    const dateText = getByText('2024년 4월 29일');
-    const tagList = getByRole('list');
-    const tagItems = getAllByRole('listitem');
+    const title = screen.getByRole('heading', {
+      level: 3,
+      name: '포스트 제목_1'
+    });
+
+    expect(title).toBeInTheDocument;
+  });
+
+  it('포스트 작성일이 표시된다.', () => {
+    render(<NotionPageItem data={parsePageItems[0]} />);
+
+    const dateText = screen.getByText('2024년 4월 29일');
+
+    expect(dateText).toBeInTheDocument;
+  });
+
+  it('태그 리스트가 표시된다.', () => {
+    render(<NotionPageItem data={parsePageItems[0]} />);
+
+    const tagList = screen.getByRole('list');
+
+    expect(tagList).toBeInTheDocument;
+  });
+
+  it('태그 아이템이 표시된다.', () => {
+    render(<NotionPageItem data={parsePageItems[0]} />);
+
+    const tagItems = screen.getAllByRole('listitem');
+    const [tagItem1, tagItem2] = tagItems;
+
+    expect(tagItems).toHaveLength(2);
+
+    expect(tagItem1.textContent).toBe('Tag Name_1-1');
+    expect(tagItem2.textContent).toBe('Tag Name_1-2');
+  });
+
+  it('태그 아이템마다 배경색 class 가 설정된다.', () => {
+    render(<NotionPageItem data={parsePageItems[0]} />);
+
+    const tagItems = screen.getAllByRole('listitem');
     const [tagItem1, tagItem2] = tagItems;
 
     const tag1 = tagItem1.querySelector('.notion-property-multi_select-item');
     const tag2 = tagItem2.querySelector('.notion-property-multi_select-item');
 
-    expect(title).toHaveTextContent('포스트 제목_1');
-    expect(dateText).toHaveRole('paragraph');
-    expect(tagList).toBeInTheDocument;
-    expect(tagItems).toHaveLength(2);
-
-    expect(tagItem1).toHaveTextContent('Tag Name_1-1');
-    expect(tagItem2).toHaveTextContent('Tag Name_1-2');
-
     expect(tag1).toHaveClass('notion-item-default');
     expect(tag2).toHaveClass('notion-item-pink');
   });
 
-  it('링크 클릭 이벤트', async () => {
+  it('포스트 아이템 클릭 시 해당 포스트 상세 페이지로 이동한다.', async () => {
     const user = userEvent.setup();
 
     render(<NotionPageItem data={parsePageItems[0]} />, {
@@ -48,20 +74,20 @@ describe('NotionPageItem 컴포넌트', () => {
     expect(mockRouter.asPath).toEqual(`/c9d287d36ecc476a9c4ec9d8b98c9a6e`);
   });
 
-  it('isCompleted 활성화', () => {
+  it(`작성 완료 포스트의 경우 '작성중' 표시가 표시되지 않는다.`, () => {
     render(<NotionPageItem data={parsePageItems[0]} />);
 
-    const completBox = screen.queryByTestId('complete-box');
+    const completBox = document.querySelector('complete-box');
 
     expect(completBox).not.toBeInTheDocument;
   });
 
-  it('isCompleted 비활성화', () => {
+  it(`작성 미완료 포스트의 경우 '작성중' 표시가 표시된다.`, () => {
     render(
       <NotionPageItem data={{ ...parsePageItems[0], isCompleted: false }} />
     );
 
-    const completBox = screen.getByTestId('complete-box');
+    const completBox = document.querySelector('complete-box');
 
     expect(completBox).toBeInTheDocument;
   });
