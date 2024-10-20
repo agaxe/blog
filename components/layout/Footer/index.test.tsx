522d9f8c9440e9399dade30a8ad7e813cb19bb84
agaxe <agaxe.dev@gmail.com>
2024-10-20 23:08:07 +0900

    update: Footer 컴포넌트 - 저작권 표시 텍스트 변경

--- components/layout/Footer/index.test.tsx
+++ components/layout/Footer/index.test.tsx
@@ -2,15 +2,16 @@ import { render, screen } from '@testing-library/react';
 import { Footer } from '.';
 
 jest.mock('@/config', () => ({
-  name: 'config-name'
+  name: 'nickname'
 }));
 
 describe('Footer 컴포넌트', () => {
-  it('config 에 설정한 name 정보가 표시된다.', async () => {
+  it('현재 년도와 닉네임을 포함한 저작권 텍스트가 표시된다.', async () => {
     render(<Footer />);
 
-    const footerTextElement = screen.getByText(`© config-name`);
+    const footerTextElement = screen.getByRole('paragraph');
 
-    expect(footerTextElement).toBeInTheDocument();
+    expect(footerTextElement).toHaveTextContent(`${new Date().getFullYear()}`);
+    expect(footerTextElement).toHaveTextContent('Nickname');
   });
 });
--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import config from '@/config';
+import { convertPascalCase } from '@/utils/convertPascalCase';
 import * as S from './styles';
 
 export const Footer = () => {
   return (
     <S.Footer>
-      <p>© {config.name}</p>
+      <p>
+        {`© ${new Date().getFullYear()}. ${convertPascalCase(
+          config.name
+        )}. All rights reserved.`}
+      </p>
     </S.Footer>
   );
 };
