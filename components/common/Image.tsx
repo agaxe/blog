47df6110e2a26af342cfb2f0dcd61e659006cdd1
agaxe <agaxe.dev@gmail.com>
2025-01-21 16:49:27 +0900

    refactor: Image 컴포넌트 - 불필요한 코드 제거

--- components/common/Image.tsx
+++ components/common/Image.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import NextImage, { ImageProps } from 'next/image';
-import styled from 'styled-components';
 
 export const Image = (props: ImageProps) => {
   return <NextImage {...props} />;
-  // eslint-disable-next-line @next/next/no-img-element
-  //return <Img src={`${String(props.src)}`} alt={props.alt} />;
 };
-
-const Img = styled.img`
-  width: inherit;
-  height: inherit;
-  object-fit: cover;
-`;
